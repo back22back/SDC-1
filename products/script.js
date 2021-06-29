8eb033d25ea1b828b0f6f2815cb9a388f6bb70ba
Jack Yang <zongchenisjack@gmail.com>
2021-06-28 23:08:10 -0700

    added new relic to measure throughput, response time, and error rate

--- products/script.js
+++ products/script.js
@@ -2,17 +2,16 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export let options = {
-  vus: 100,
+  vus: 10,
   duration: '10s',
   // noConnectionReuse: true
 };
 
 export default function () {
-  // for (var i = 0; i < 100; i++) {
-    let res = http.get(`http://localhost:3000/products/8000`);
-    check(res, {
-      "is status 200": (r) => r.status === 200
-    });
+  let rand = Math.floor(Math.random() * 1000000);
+  let res = http.get(`http://localhost:3000/products/${rand}`);
+  check(res, {
+    "is status 200": (r) => r.status === 200
+  });
     // sleep(1);
-  // }
 }
